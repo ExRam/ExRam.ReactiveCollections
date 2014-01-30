5531476e58928478cebd852058aa04784e566a41
Blewzman <dw@exram.de>
2014-01-30 12:22:47 +0100

    Added WithChanges Extension method

---
+++ ExRam.ReactiveCollections/Extensions/ReactiveCollectionExtensions (WithChanges).cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Reactive.Linq;
+
+namespace ExRam.ReactiveCollections
+{
+    public static partial class ReactiveCollectionExtensions
+    {
+        public static IReactiveCollection<TNotification, TSource> WithChanges<TNotification, TSource>(this IReactiveCollection<TNotification, TSource> source, Func<IObservable<TNotification>, IObservable<TNotification>> changesTransformation) where TNotification : ICollectionChangedNotification<TSource>
+        {
+            Contract.Requires(source != null);
+            Contract.Requires(changesTransformation != null);
+
+            return changesTransformation(source.Changes)
+                .ToReactiveCollection<TNotification, TSource>();
+        }
+    }
+}
