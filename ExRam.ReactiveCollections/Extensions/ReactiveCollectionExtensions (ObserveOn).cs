ad4bb428b65b31c3b2cb4cb62d1f7237beffe525
Blewzman <dw@exram.de>
2014-01-30 12:27:08 +0100

    ObserveOn-Extension: Keep it simple!

--- ExRam.ReactiveCollections/Extensions/ReactiveCollectionExtensions (ObserveOn).cs
+++ ExRam.ReactiveCollections/Extensions/ReactiveCollectionExtensions (ObserveOn).cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Specialized;
-using System.Diagnostics.Contracts;
-using System.Linq;
+﻿using System.Diagnostics.Contracts;
 using System.Reactive.Linq;
 using System.Threading;
 
@@ -9,35 +6,13 @@ namespace ExRam.ReactiveCollections
 {
     public static partial class ReactiveCollectionExtensions
     {
-        #region ObserveOnReactiveCollection
-        private sealed class ObserveOnReactiveCollection<TNotification, TSource> : IReactiveCollection<TNotification, TSource>
-            where TNotification : ICollectionChangedNotification<TSource>
-        {
-            private readonly IObservable<TNotification> _changes;
-
-            public ObserveOnReactiveCollection(IReactiveCollection<TNotification, TSource> source, SynchronizationContext syncContext)
-            {
-                Contract.Requires(source != null);
-
-                this._changes = source.Changes
-                    .ObserveOn(syncContext);
-            }
-
-            public IObservable<TNotification> Changes
-            {
-                get
-                {
-                    return this._changes;
-                }
-            }
-        }
-        #endregion
-
         public static IReactiveCollection<TNotification, TSource> ObserveOn<TNotification, TSource>(this IReactiveCollection<TNotification, TSource> source, SynchronizationContext syncContext) where TNotification : ICollectionChangedNotification<TSource>
         {
             Contract.Requires(source != null);
 
-            return new ObserveOnReactiveCollection<TNotification, TSource>(source, syncContext);
+            return source.Changes
+                .ObserveOn(syncContext)
+                .ToReactiveCollection<TNotification, TSource>();
         }
     }
 }
