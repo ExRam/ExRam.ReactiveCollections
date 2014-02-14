be5165b126c31d9a622532c105e52cee127ea442
Blewzman <dw@exram.de>
2014-02-14 12:57:55 +0100

    New: IConnectableReactiveCollection, ToConnectableReactiveCollection, Publish.

---
+++ ExRam.ReactiveCollections/Extensions/ObservableExtensions (ToConnectableReactiveCollection).cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.Contracts;
+using ExRam.ReactiveCollections;
+
+namespace System.Reactive.Linq
+{
+    public static partial class ObservableExtensions
+    {
+        #region ToReactiveCollectionImpl
+        private sealed class ToConnectableReactiveCollectionImpl<TNotification, T> : IConnectableReactiveCollection<TNotification, T>
+            where TNotification : ICollectionChangedNotification<T>
+        {
+            private readonly Func<IDisposable> _connectFunction;
+            private readonly IObservable<TNotification> _changes;
+
+            public ToConnectableReactiveCollectionImpl(IObservable<TNotification> changes, Func<IDisposable> connectFunction)
+            {
+                Contract.Requires(changes != null);
+                Contract.Requires(connectFunction != null);
+
+                this._changes = changes
+                    .Normalize<TNotification, T>();
+
+                this._connectFunction = connectFunction;
+            }
+
+            IObservable<TNotification> IReactiveCollection<TNotification, T>.Changes
+            {
+                get
+                {
+                    return this._changes;
+                }
+            }
+
+            public IDisposable Connect()
+            {
+                return this._connectFunction();
+            }
+        }
+        #endregion
+
+        public static IConnectableReactiveCollection<TNotification, T> ToConnectableReactiveCollection<TNotification, T>(this IObservable<TNotification> changesObservable, Func<IDisposable> connectFunction)
+            where TNotification : ICollectionChangedNotification<T>
+        {
+            Contract.Requires(changesObservable != null);
+            Contract.Requires(connectFunction != null);
+
+            return new ToConnectableReactiveCollectionImpl<TNotification, T>(changesObservable, connectFunction);
+        }
+    }
+}
---
+++ ExRam.ReactiveCollections/Interfaces/IConnectableReactiveCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace ExRam.ReactiveCollections
+{
+    [ContractClass(typeof(ConnectableReactiveCollectionContracts<,>))]
+    public interface IConnectableReactiveCollection<out TNotification, T> : IReactiveCollection<TNotification, T>
+        where TNotification : ICollectionChangedNotification<T>
+    {
+        IDisposable Connect();
+    }
+
+    [ContractClassFor(typeof(IConnectableReactiveCollection<,>))]
+    public abstract class ConnectableReactiveCollectionContracts<TNotification, T> : IConnectableReactiveCollection<TNotification, T>
+         where TNotification : ICollectionChangedNotification<T>
+    {
+        public abstract IObservable<TNotification> Changes
+        {
+            get;
+        }
+
+        public IDisposable Connect()
+        {
+            Contract.Ensures(Contract.Result<IDisposable>() != null);
+
+            return default(IDisposable);
+        }
+    }
+}
