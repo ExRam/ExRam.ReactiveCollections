1c49abc967526ef07ec13c6034c8972683ae69a3
Daniel Weber <dw@exram.de>
2014-11-27 12:12:14 +0100

    New: SubscribeOn extension method.

---
+++ ExRam.ReactiveCollections/Extensions/ReactiveCollectionExtensions (SubscribeOn).cs
@@ -0,0 +1,28 @@
+﻿// (c) Copyright 2014 ExRam GmbH & Co. KG http://www.exram.de
+//
+// Licensed using Microsoft Public License (Ms-PL)
+// Full License description can be found in the LICENSE
+// file.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace ExRam.ReactiveCollections
+{
+    public static partial class ReactiveCollectionExtensions
+    {
+        public static IReactiveCollection<TNotification, TSource> SubscribeOn<TNotification, TSource>(this IReactiveCollection<TNotification, TSource> source, IScheduler scheduler) where TNotification : ICollectionChangedNotification<TSource>
+        {
+            Contract.Requires(source != null);
+            Contract.Requires(scheduler != null);
+            Contract.Ensures(Contract.Result<IReactiveCollection<TNotification, TSource>>() != null);
+
+            return source.Changes
+                .SubscribeOn(scheduler)
+                .ToReactiveCollection<TNotification, TSource>();
+        }
+    }
+}
