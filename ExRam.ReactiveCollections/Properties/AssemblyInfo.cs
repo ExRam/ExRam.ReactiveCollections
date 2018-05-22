ea7348b53b78adea763919d957c8cadd88a826cc
Daniel Weber <dw@exram.de>
2018-05-22 11:53:12 +0200

    AssemblyInfo cleanup, Version bump.

--- ExRam.ReactiveCollections/Properties/AssemblyInfo.cs
+++ ExRam.ReactiveCollections/Properties/AssemblyInfo.cs
@@ -1,30 +1,3 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
-// Allgemeine Informationen über eine Assembly werden über folgende 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("ExRam.ReactiveCollections")]
-[assembly: AssemblyDescription("ExRam.ReactiveCollections Library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ExRam EDV GmbH & Co. KG")]
-[assembly: AssemblyProduct("ExRam.ReactiveCollections")]
-[assembly: AssemblyCopyright("Copyright © 2013-2014 ExRam EDV GmbH & Co. KG")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("de")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// durch Einsatz von '*', wie in nachfolgendem Beispiel:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("ExRam.ReactiveCollections.Tests")]
